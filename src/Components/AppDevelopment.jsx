21d02402d1b4dd8bfa74560b4115c40462557ad9
Kanchan Chaudhary <kanchanchaudhary@Kanchans-MacBook-Pro.local>
2021-10-15 18:18:35 +0530

    first commit

---
+++ src/Components/AppDevelopment.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Hero from "./Hero";
+import ServicesCards from "./ServicesCards";
+import andriod from "../Images/android.jpg";
+import iOS from "../Images/iOS.jpg";
+
+const AppDevelopment = () => {
+
+    return (
+        <>
+        <Hero hero_text="World is coming together with mobile apps"/>
+        <div className="container-fluid">
+            <div className="row mt-5">
+                <div className="col-md-10 mx-auto"></div>
+                <div className="row mb-5">
+                    <ServicesCards servicesHeadingText="Android App Development"/>
+                    <div className="col-md-5 mx-auto">
+                        <img className="animated2 app-service__Image rounded" src={andriod} alt="..." />
+                    </div>
+                </div>
+                <div className="row mb-5">
+                     <div className="col-md-5 mx-auto">
+                         <img className="animated2 app-service__Image rounded" src={iOS} alt="..." />
+                     </div>
+                     <ServicesCards servicesHeadingText="iOS App Development"/>
+                </div>
+            </div>
+        </div>
+         </>
+    )
+}
+
+export default AppDevelopment;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Common from "./Common";
+import web from "../Images/blogging.svg";
+
+const Home = () => {
+  return (
+  <>
+  <Common subtext="People don’t just stumble into our office. It is through a conscious, concerted effort to find the best team. As promised, we’re the most professional website designing company."  text2="Meet" buttonName="Get Started" text1="Make your business shine on the web with" visit="/Service" imgsrc={web} />
+  </>
+  );
+};
+
+export default Home;
